af4eb475ea1d6a997e92658a84bc81e4b1e86feb
River Bryant <river.bryant@ibethel.org>
2014-06-06 12:33:46 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2ff447c82a45e3258b27f071fbfbae0769bae56714915eb35ba38e2e4c2ce0e8b23f5bbcc7491abbf04dba26201d4a470c885283aa6d4d3148cb7af84ca8ded5'
